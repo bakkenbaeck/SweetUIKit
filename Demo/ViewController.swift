0fd62c704e24d662ffa02bfc770488371e751921
Igor Ranieri Elland <igor@elland.me>
2016-08-29 16:28:43 +0200

    Add default low priority constraints to make collection view fill its container

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,10 +7,6 @@ class ViewController: SweetCollectionViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = .magentaColor()
 
-        self.collectionView.topAnchor.constraintEqualToAnchor(self.view.topAnchor).active = true
-        self.collectionView.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
-        self.collectionView.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor).active = true
-        self.collectionView.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
         self.collectionView.register(UICollectionViewCell.self)
 
         self.data = ["123", "321", "lol"]
