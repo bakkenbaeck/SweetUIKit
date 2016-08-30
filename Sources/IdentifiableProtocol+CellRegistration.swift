1bc192eec54f3d7d703ddddbe7fed5fb7ba8b0c3
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-08-30 13:57:14 +0200

    Add support for registering nibs (#10)

--- Sources/IdentifiableProtocol+CellRegistration.swift
+++ Sources/IdentifiableProtocol+CellRegistration.swift
@@ -25,11 +25,19 @@ public extension UITableView {
     public func register(cellClass: UITableViewCell.Type) {
         self.registerClass(cellClass.self, forCellReuseIdentifier: cellClass.reuseIdentifier)
     }
+
+    public func registerNib(cellClass: UITableViewCell.Type) {
+        self.registerNib(UINib.init(nibName: String(cellClass), bundle: nil), forCellReuseIdentifier: cellClass.reuseIdentifier)
+    }
 }
 
 public extension UICollectionView {
     public func register(cellClass: UICollectionViewCell.Type) {
         self.registerClass(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+    }
+
+    public func registerNib(cellClass: UICollectionViewCell.Type) {
+        self.registerNib(UINib.init(nibName: String(cellClass), bundle: nil), forCellWithReuseIdentifier: cellClass.reuseIdentifier)
     }
 }
 #endif
