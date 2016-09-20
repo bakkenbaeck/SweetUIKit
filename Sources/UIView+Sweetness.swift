2efa43a2c7a0009d691f8a1013970c9157f4e361
Elvis Nuñez <elvisnunez@me.com>
2016-09-20 17:44:07 +0200

    Move UIToolbar to it's own file

--- Sources/UIView+Sweetness.swift
+++ Sources/UIView+Sweetness.swift
@@ -39,15 +39,4 @@ public extension UIView {
     }
 }
 
-public extension UIToolbar {
-
-    public func attachToTop() {
-        guard let superview = self.superview else { return }
-
-        self.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
-        self.leftAnchor.constraint(equalTo: superview.leftAnchor).isActive = true
-        self.rightAnchor.constraint(equalTo: superview.rightAnchor).isActive = true
-    }
-}
-
 #endif
