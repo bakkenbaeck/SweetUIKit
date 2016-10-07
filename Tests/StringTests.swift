b85c8dfece82e9f29c9e2dde1dc7511193b26f3f
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-10-07 10:13:11 +0200

    Add method to calculate the width of a String (#26)

---
+++ Tests/StringTests.swift
@@ -0,0 +1,21 @@
+import UIKit
+import XCTest
+
+class StringTests: XCTestCase {
+    func testWidth() {
+        let smallFont = UIFont.systemFont(ofSize: 15)
+        let bigFont = UIFont.boldSystemFont(ofSize: 30)
+
+        var string = ""
+        XCTAssertEqual(string.width(for: smallFont), 0)
+        XCTAssertEqual(string.width(for: bigFont), 0)
+
+        string = "a"
+        XCTAssertEqual(floor(string.width(for: smallFont)), 8)
+        XCTAssertEqual(floor(string.width(for: bigFont)), 16)
+
+        string = "aa"
+        XCTAssertEqual(floor(string.width(for: smallFont)), 16)
+        XCTAssertEqual(floor(string.width(for: bigFont)), 33)
+    }
+}
