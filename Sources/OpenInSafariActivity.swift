97bc23409b55749205769b375135a56757443f40
Elvis Nuñez <elvisnunez@me.com>
2016-10-25 11:47:00 +0200

    Mark unavailable classes

--- Sources/OpenInSafariActivity.swift
+++ Sources/OpenInSafariActivity.swift
@@ -1,54 +1,56 @@
 import UIKit
 
-class OpenInSafariActivity: UIActivity {
-    fileprivate var url: URL?
+#if os(iOS)
+    class OpenInSafariActivity: UIActivity {
+        fileprivate var url: URL?
 
-    override var activityType: UIActivityType? {
-        return UIActivityType(String(describing: self.classForCoder))
-    }
+        override var activityType: UIActivityType? {
+            return UIActivityType(String(describing: self.classForCoder))
+        }
 
-    override var activityTitle: String? {
-        let defaultTitle = Bundle(for: self.classForCoder).localizedString(forKey: "Open in Safari", value: "Open in Safari Default", table: "OpenInSafariActivity")
-        let title = Bundle.main.localizedString(forKey: "Open in Safari", value: defaultTitle, table: nil)
+        override var activityTitle: String? {
+            let defaultTitle = Bundle(for: self.classForCoder).localizedString(forKey: "Open in Safari", value: "Open in Safari Default", table: "OpenInSafariActivity")
+            let title = Bundle.main.localizedString(forKey: "Open in Safari", value: defaultTitle, table: nil)
 
-        return title
-    }
+            return title
+        }
 
-    override var activityImage: UIImage? {
-        return UIImage(named: "Safari", in: Bundle(for: self.classForCoder), compatibleWith: nil)
-    }
+        override var activityImage: UIImage? {
+            return UIImage(named: "Safari", in: Bundle(for: self.classForCoder), compatibleWith: nil)
+        }
 
-    override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
-        for item in activityItems {
-            if let item = item as? URL {
-                return UIApplication.shared.canOpenURL(item)
+        override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
+            for item in activityItems {
+                if let item = item as? URL {
+                    return UIApplication.shared.canOpenURL(item)
+                }
+            }
+
+            return false
+        }
+
+        override func prepare(withActivityItems activityItems: [Any]) {
+            for item in activityItems {
+                if let item = item as? URL {
+                    self.url = item
+                    break
+                }
             }
         }
 
-        return false
-    }
-
-    override func prepare(withActivityItems activityItems: [Any]) {
-        for item in activityItems {
-            if let item = item as? URL {
-                self.url = item
-                break
-            }
-        }
-    }
-
-    override func perform() {
-        if let url = self.url {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(url, options: [:]) { completed in
+        override func perform() {
+            if let url = self.url {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:]) { completed in
+                        self.activityDidFinish(completed)
+                    }
+                } else {
+                    let completed = UIApplication.shared.openURL(url)
                     self.activityDidFinish(completed)
                 }
             } else {
-                let completed = UIApplication.shared.openURL(url)
-                self.activityDidFinish(completed)
+                self.activityDidFinish(false)
             }
-        } else {
-            self.activityDidFinish(false)
         }
     }
-}
+#endif
