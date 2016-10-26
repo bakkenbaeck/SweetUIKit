9453967055d1ef2efb142119640443b2fc1cc6f7
Elvis Nuñez <elvisnunez@me.com>
2016-10-26 10:50:16 +0200

    Rename
    - Rename files to be Sweet instead of Sweetness
    - Move UITableView and UICollectionView extensions to their own files

---
+++ Sources/UICollectionView+Sweet.swift
@@ -0,0 +1,17 @@
+#if os(iOS) || os(tvOS)
+    import UIKit
+
+    public extension UICollectionView {
+        public func register(_ cellClass: UICollectionViewCell.Type) {
+            self.register(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+        }
+
+        public func registerNib(_ cellClass: UICollectionViewCell.Type) {
+            self.register(UINib.init(nibName: cellClass.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+        }
+
+        public func dequeue<T where T: Identifiable, T: UICollectionViewCell>(_ cellClass: T.Type, for indexPath: IndexPath) -> T {
+            return self.dequeueReusableCell(withReuseIdentifier: cellClass.reuseIdentifier, for: indexPath) as! T
+        }
+    }
+#endif
