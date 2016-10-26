be123f6ba9c53edb394f17c05d1bb258ecbc9583
Igor Ranieri Elland <igor@elland.me>
2016-10-26 15:03:52 +0200

    Added IndexPath extensions; formerly SweetFoundation.

--- Sources/IndexPath+Sweet.swift
+++ Sources/IndexPath+Sweet.swift
@@ -2,6 +2,32 @@
 import UIKit
 
 public extension IndexPath {
+    public enum Direction {
+        case same
+        case before
+        case ahead
+    }
+
+    fileprivate func comparePosition(_ ownRow: Int, otherRow: Int) -> Direction {
+        if ownRow == otherRow {
+            return .same
+        } else if ownRow < otherRow {
+            return .before
+        } else {
+            return .ahead
+        }
+    }
+
+    public func comparePosition(to indexPath: IndexPath) -> Direction {
+        if self.section == indexPath.section {
+            return self.comparePosition(self.row, otherRow: indexPath.row)
+        } else if self.section < indexPath.section {
+            return .before
+        } else {
+            return .ahead
+        }
+    }
+
     public func indexPaths(collectionView: UICollectionView) -> [IndexPath] {
         var indexPaths = [IndexPath]()
 
---
+++ Tests/IndexPathTests.swift
@@ -0,0 +1,31 @@
+import UIKit
+import XCTest
+
+class IndexPathTests: XCTestCase {
+    func testPositionComparison() {
+        let firstPath = IndexPath(row: 0, section: 0)
+        let secondPath = IndexPath(row: 1, section: 0)
+        let thirdPath = IndexPath(row: 0, section: 1)
+        let fourthPath = IndexPath(row: 1, section: 1)
+
+        XCTAssert(firstPath.comparePosition(to: firstPath) == .same)
+        XCTAssert(firstPath.comparePosition(to: secondPath) == .before)
+        XCTAssert(firstPath.comparePosition(to: thirdPath) == .before)
+        XCTAssert(firstPath.comparePosition(to: fourthPath) == .before)
+
+        XCTAssert(secondPath.comparePosition(to: firstPath) == .ahead)
+        XCTAssert(secondPath.comparePosition(to: secondPath) == .same)
+        XCTAssert(secondPath.comparePosition(to: thirdPath) == .before)
+        XCTAssert(secondPath.comparePosition(to: fourthPath) == .before)
+
+        XCTAssert(thirdPath.comparePosition(to: firstPath) == .ahead)
+        XCTAssert(thirdPath.comparePosition(to: secondPath) == .ahead)
+        XCTAssert(thirdPath.comparePosition(to: thirdPath) == .same)
+        XCTAssert(thirdPath.comparePosition(to: fourthPath) == .before)
+
+        XCTAssert(fourthPath.comparePosition(to: firstPath) == .ahead)
+        XCTAssert(fourthPath.comparePosition(to: secondPath) == .ahead)
+        XCTAssert(fourthPath.comparePosition(to: thirdPath) == .ahead)
+        XCTAssert(fourthPath.comparePosition(to: fourthPath) == .same)
+    }
+}
