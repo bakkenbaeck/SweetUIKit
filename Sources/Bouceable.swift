a733be57e2eb02781a864eaa2450bd302516b62c
Igor Ranieri Elland <Elland@users.noreply.github.com>
2016-10-31 11:12:44 +0100

    Add Bounceable protocol (#43)
    * Add Bouceable protocol.
    
    - To test it, just tap and hold on the collection view cell.
    
    * As usual, forgot about access.

---
+++ Sources/Bouceable.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+public protocol Bounceable: class {
+    func startBouncing()
+    func stopBouncing()
+
+    var transform: CGAffineTransform { get set }
+    var layer: CALayer { get }
+}
+
+public extension Bounceable {
+    fileprivate func degreesToRadians(_ degrees: Double) -> Double {
+        return M_PI * degrees / 180.0
+    }
+    
+    public func startBouncing() {
+        let animationRotationDegrees = 1.25
+        let random = Double(arc4random_uniform(500))
+        let r = (random / 500.0) + 0.5
+
+        let leftWobble = CGAffineTransform(rotationAngle: CGFloat(self.degreesToRadians((animationRotationDegrees * -1.0) - r)))
+        let rightWobble = CGAffineTransform(rotationAngle: CGFloat(self.degreesToRadians(animationRotationDegrees + r)))
+
+        self.transform = leftWobble
+        self.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        let options = UIViewAnimationOptions(arrayLiteral: .allowUserInteraction, .repeat, .autoreverse)
+        UIView.animate(withDuration: 0.15, delay: 0.0, options: options, animations: {
+            self.transform = rightWobble
+        })
+    }
+
+    public func stopBouncing() {
+        self.layer.removeAllAnimations()
+        self.transform = .identity
+    }
+}
