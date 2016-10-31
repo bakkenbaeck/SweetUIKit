6b44ed54484a6af498c6f4fe3eaa9859ff67db2d
Igor Ranieri Elland <igor@elland.me>
2016-10-31 11:04:55 +0100

    Add Bouceable protocol.
    - To test it, just tap and hold on the collection view cell.

--- iOSDemo/CollectionViewCell.swift
+++ iOSDemo/CollectionViewCell.swift
@@ -1,13 +1,15 @@
 import UIKit
 import SweetUIKit
 
-class CollectionViewCell: UICollectionViewCell {
+class CollectionViewCell: UICollectionViewCell, Bounceable {
     lazy var label: UILabel = {
         let view = UILabel(withAutoLayout: true)
         view.textAlignment = .center
 
         return view
     }()
+
+    fileprivate var isBouncing = false
 
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -17,9 +19,22 @@ class CollectionViewCell: UICollectionViewCell {
         self.label.leftAnchor.constraint(equalTo: self.contentView.leftAnchor).isActive = true
         self.label.rightAnchor.constraint(equalTo: self.contentView.rightAnchor).isActive = true
         self.label.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(toggleBouncing))
+        self.contentView.addGestureRecognizer(longPress)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("The method `init?(coder)` is not implemented for this class.")
+    }
+
+    func toggleBouncing() {
+        if self.isBouncing {
+            self.isBouncing = false
+            self.stopBouncing()
+        } else {
+            self.isBouncing = true
+            self.startBouncing()
+        }
     }
 }
