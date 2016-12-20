9e81b121abd4fabb16f8c1b5310c912086eec9ec
Elvis Nuñez <elvisnunez@me.com>
2016-12-20 09:05:14 +0100

    Add UILocalNotification + Sweet

---
+++ Sources/UILocalNotification+Sweet.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+public extension UILocalNotification {
+    static let idKey = "idKey"
+    static let fireDateKey = "fireDateKey"
+
+    public static func create(_ id: String, fireDate: Date, soundName: String? = nil, message: String, actionTitle: String? = nil) {
+        let notification = UILocalNotification()
+        notification.soundName = soundName
+
+        notification.fireDate = fireDate
+        notification.timeZone = TimeZone.current
+        notification.alertBody = message
+        notification.alertAction = actionTitle
+        if actionTitle == nil {
+            notification.hasAction = false
+        }
+
+        var userInfo = [AnyHashable: Any]()
+        userInfo[UILocalNotification.idKey] = id
+        userInfo[UILocalNotification.fireDateKey] = Date()
+        notification.userInfo = userInfo
+
+        UIApplication.shared.scheduleLocalNotification(notification)
+    }
+
+    public static func find(_ id: String) -> UILocalNotification? {
+        let notifications = UIApplication.shared.scheduledLocalNotifications ?? [UILocalNotification]()
+        for notification in notifications {
+            if let userInfo = notification.userInfo {
+                if let key = userInfo[UILocalNotification.idKey] as? String {
+                    if key == id {
+                        return notification
+                    }
+                }
+            }
+        }
+
+        return nil
+    }
+
+    public static func delete(_ id: String) {
+        if let notification = find(id) {
+            UIApplication.shared.cancelLocalNotification(notification)
+        }
+    }
+}
