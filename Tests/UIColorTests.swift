59370c0cb538833ebca2db543a61011661ea2a5c
Elvis Nuñez <elvisnunez@me.com>
2017-01-20 00:20:38 +0100

    Add test with alpha

--- Tests/UIColorTests.swift
+++ Tests/UIColorTests.swift
@@ -40,5 +40,6 @@ class Tests: XCTestCase {
     func testRGBConvenience() {
         XCTAssertTrue(UIColor(r: 0, g: 0, b: 0).isEqual(to: UIColor.black))
         XCTAssertTrue(UIColor(r: 255, g: 255, b: 255).isEqual(to: UIColor.white))
+        XCTAssertTrue(UIColor(r: 0, g: 0, b: 0, a: 50).isEqual(to: UIColor.black.withAlphaComponent(0.5)))
     }
 }
