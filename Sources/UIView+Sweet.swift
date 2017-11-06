924e42c03415637966693445e0617c87524baec2
Igor Ranieri Elland <igor@elland.me>
2017-11-06 11:11:02 +0100

    Cleaned up code, documented method.

--- Sources/UIView+Sweet.swift
+++ Sources/UIView+Sweet.swift
@@ -5,7 +5,7 @@ public extension UIView {
     /**
      Disturbs the view. Useful for getting the user's attention when something changed.
      */
-    open func disturb() {
+    public func disturb() {
         self.transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
 
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 150, options: [.curveEaseOut, .beginFromCurrentState, .allowUserInteraction], animations: {
@@ -16,7 +16,7 @@ public extension UIView {
     /**
      Shakes the view. Useful for displaying failures to users.
      */
-    open func shake() {
+    public func shake() {
         self.transform = CGAffineTransform(translationX: 10, y: 0)
 
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 50, options: [.curveEaseOut, .beginFromCurrentState, .allowUserInteraction], animations: {
@@ -29,7 +29,7 @@ public extension UIView {
         self.translatesAutoresizingMaskIntoConstraints = !autoLayout
     }
 
-    open func fillSuperview(with insets: UIEdgeInsets = UIEdgeInsets.zero) {
+    public func fillSuperview(with insets: UIEdgeInsets = UIEdgeInsets.zero) {
         guard let superview = self.superview else { return }
 
         let constants = [
@@ -45,18 +45,18 @@ public extension UIView {
         }
     }
 
-    open func set(height: CGFloat) {
+    public func set(height: CGFloat) {
         self.heightAnchor.constraint(equalToConstant: height).isActive = true
     }
 
-    open func set(width: CGFloat) {
+    public func set(width: CGFloat) {
         self.widthAnchor.constraint(equalToConstant: width).isActive = true
     }
 
     /// Attach a view to the top of its superview or view controller's top layout guide, if one is supplied
     ///
     /// - Parameter viewController: If not nil, attach the view to the bottom of its top layout guide.
-    open func attachToTop(viewController: UIViewController? = nil) {
+    public func attachToTop(viewController: UIViewController? = nil) {
         guard let superview = self.superview else { return }
 
         self.topAnchor.constraint(equalTo: viewController?.topLayoutGuide.bottomAnchor ?? superview.topAnchor).isActive = true
@@ -66,9 +66,11 @@ public extension UIView {
 
     /// Helper method to instantiate a nib file.
     ///
+    /// - Parameter nibName: The name of the nib to be loaded. If none is passed, it uses the class name. Defaults to nil.
     /// - Parameter bundle: The bundle where the nib is located, by default we'll use the main bundle.
+    ///
     /// - Returns: Returns an instance of the nib as a UIView.
-    open class func instanceFromNib<T: UIView>(nibName: String? = nil, bundle: Bundle = .main) -> T {
+    public class func instanceFromNib<T: UIView>(nibName: String? = nil, bundle: Bundle = .main) -> T {
         let name = nibName ?? String(describing: T.self)
         return UINib(nibName: name, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as! T
     }
